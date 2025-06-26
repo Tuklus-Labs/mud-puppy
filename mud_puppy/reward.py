11f846255ec7e3021f80bca23c96fd4346abd899
apollosenvy <strange.twentyone@gmail.com>
2025-06-25 20:08:30 -0700

    Delete mud_puppy directory

--- mud_puppy/reward.py
+++
@@ -1,9 +0,0 @@
-"""Reward modeling utilities."""
-
-from .config import TrainingConfig
-
-
-def train_reward_model(config: TrainingConfig):
-    """Placeholder for Reward Modeling/PRM training."""
-    raise NotImplementedError("Reward modeling is not implemented yet")
-
